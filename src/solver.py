535139fb81aa1edeb734353919a7c751171e9494
Thivejan Neranjan <35178093+Thive-N@users.noreply.github.com>
2023-06-21 20:08:36 +0100

    color trap cases

--- src/solver.py
+++ src/solver.py
@@ -2,6 +2,7 @@ import validity
 import utils
 import random
 import generate
+import copy
 
 
 def solve(board: list[list[int]]) -> list[list[int]]:
@@ -22,18 +23,38 @@ def solve(board: list[list[int]]) -> list[list[int]]:
     # emptycells = validity.getemptysquares(board)
     # if len(emptycells) == 0:
     #     return board
-    maxiterations = 100
+    maxiterations = 10000
+    previousboard = copy.deepcopy(board)
+    savestate = None
     while True:
         maxiterations -= 1
         if maxiterations == 0:
             print("max iterations reached")
             break
         board = solveEasyCases(board)
-        board = solveSingleCandidateCases(board)
+
         emptycells = validity.getemptysquares(board)
         if len(emptycells) == 0:
             break
 
+        board = solveSingleCandidateCases(board)
+
+        emptycells = validity.getemptysquares(board)
+        if len(emptycells) == 0:
+            break
+
+        if maxiterations < 200:
+            solveColorTrap(board)
+
+        if previousboard == board:
+            if savestate is None:
+                savestate = copy.deepcopy(board)
+            try:
+                board = solveProbabilisticCases(board)
+            except:
+                board = copy.deepcopy(savestate)
+
+        previousboard = copy.deepcopy(board)
     return board
 
 
@@ -95,10 +116,111 @@ def solveSingleCandidateCases(board: list[list[int]]) -> list[list[int]]:
     return board
 
 
+def solveProbabilisticCases(board: list[list[int]]) -> list[list[int]]:
+    """Solve probabilistic cases of a sudoku board.
+
+    Args:
+        board (list[list[int]]): The board to solve.
+
+    Returns:
+        list[list[int]]: The solved board.
+
+    Note:
+        Empty cells are represented by the Nonetype.
+        boards are considered valid
+        this part of the algorithm needs to only attempt to fix a single cell
+    """
+
+    emptycells = validity.getemptysquares(board)
+    randomcell = random.choice(emptycells)
+    valids = validity.validatposition(board, randomcell[0], randomcell[1])
+    randomvalue = random.choice(valids)
+    board[randomcell[0]][randomcell[1]] = randomvalue
+    return board
+
+
+def solveColorTrap(board: list[list[int]], candidate: int = 0) -> list[list[int]]:
+    """Solve a board using the color wrap algorithm.
+
+    Args:
+        board (list[list[int]]): The board to solve.
+
+    Returns:
+        list[list[int]]: The solved board.
+
+    Note:
+        Empty cells are represented by the Nonetype.
+        boards are considered valid
+    """
+    if candidate == 10:
+        return board
+
+    candidatecells = validity.getCellsWithCandidates(board, candidate)
+    conjugatepairs = validity.getConjugatePairs(
+        board, candidatecells, candidate)
+
+    # if len(conjugatepairs) == 0:
+    #     return solveColorTrap(board, candidate + 1)
+    networks: list[dict[tuple[int, int], set[tuple[int, int]]]] = []
+
+    while len(conjugatepairs) > 0:
+        updated = False
+        for network in networks:
+            for pairL, pairR in conjugatepairs:
+                if pairL in network:
+                    network[pairL].add(pairR)
+                    network[pairR] = {pairL}
+                    conjugatepairs.remove((pairL, pairR))
+                    updated = True
+                    break
+                elif pairR in network:
+                    network[pairR].add(pairL)
+                    network[pairL] = {pairR}
+                    conjugatepairs.remove((pairL, pairR))
+                    updated = True
+                    break
+
+        if not updated and len(conjugatepairs) > 0:
+            pair = conjugatepairs.pop()
+            newnetwork = {pair[0]: {pair[1]}}
+            networks.append(newnetwork)
+
+    for network in networks:
+        # create colormap
+        colormap: dict[tuple[int, int], bool] = {}
+        for cell, subnetwork in network.items():
+            if cell in colormap:
+                for subcell in subnetwork:
+                    colormap[subcell] = not colormap[cell]
+            else:
+                colormap[cell] = True
+                for subcell in subnetwork:
+                    colormap[subcell] = False
+
+        for candidatecell in candidatecells:
+            if candidatecell in colormap:
+                continue
+            else:
+                visf = False
+                vist = True
+                visible = validity.getAffectingSquaresNonComplete(
+                    board, candidatecell[0], candidatecell[1])
+                for cell in visible:
+                    if cell in colormap:
+                        if colormap[cell]:
+                            vist = True
+                        else:
+                            visf = True
+
+                if visf and vist:
+                    if candidatecell in validity.colorvalueremove:
+                        validity.colorvalueremove[candidatecell].add(candidate)
+                    else:
+                        validity.colorvalueremove[candidatecell] = {candidate}
+
+    solveColorTrap(board, candidate + 1)
+
+
 if __name__ == "__main__":
-    board = generate.generatepartial(percentageRemoved=0.4)
-    utils.prettyprint(board)
-    print()
-    solved = solve(board)
-    utils.prettyprint(solved)
-    print(utils.export(solved))
+    board = generate.generatepartial(percentageRemoved=0.1)
+    utils.prettyprint(solve(board))
--- src/validity.py
+++ src/validity.py
@@ -1,3 +1,7 @@
+import copy
+colorvalueremove: dict[tuple[int, int], set[int]] = {}
+
+
 def boardValid(board: list[list[int]]) -> bool:
     """Check if the board is valid.
 
@@ -67,6 +71,10 @@ def validatposition(board: list[list[int]], row: int, column: int) -> list[int]:
         Empty cells are represented by the Nonetype.
     """
     valid = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    if board[row][column] in colorvalueremove:
+        valid = list(
+            filter(lambda x: x not in colorvalueremove[board[row][column]], valid))
+
     if board[row][column] is not None:
         return []
 
@@ -157,3 +165,100 @@ def getAffectingSquaresBox(board: list[list[int]], row: int, col: int) -> list[t
                 affected.append((boxrow+i, boxcolumn+j))
 
     return affected
+
+
+def getAffectingSquaresNonComplete(board: list[list[int]], row: int, col: int) -> list[tuple[int, int]]:
+    affected = []
+    for i in range(9):
+        if i != row and board[i][col] is None:
+            affected.append((i, col))
+        if i != col and board[row][i] is None:
+            affected.append((row, i))
+
+    boxrow = row - row % 3
+    boxcolumn = col - col % 3
+    for i in range(3):
+        for j in range(3):
+            if boxrow+i != row and boxcolumn+j != col and board[boxrow+i][boxcolumn+j] is None:
+                affected.append((boxrow+i, boxcolumn+j))
+
+    return affected
+
+
+def getCellsWithCandidates(board: list[list[int]], candidate: int) -> list[tuple[int, int]]:
+    cells = []
+    for row in range(9):
+        for col in range(9):
+            if candidate in validatposition(board, row, col):
+                cells.append((row, col))
+    return cells
+
+
+def inSameRow(cell1: tuple[int, int], cell2: tuple[int, int]) -> bool:
+    return cell1[0] == cell2[0]
+
+
+def inSameColumn(cell1: tuple[int, int], cell2: tuple[int, int]) -> bool:
+    return cell1[1] == cell2[1]
+
+
+def inSameBox(cell1: tuple[int, int], cell2: tuple[int, int]) -> bool:
+    return cell1[0] // 3 == cell2[0] // 3 and cell1[1] // 3 == cell2[1] // 3
+
+
+def getConjugatePair(board: list[list[int]], candidateCell: tuple[int, int], value: int) -> list[tuple[tuple[int, int], tuple[int, int]]]:
+    """Get the conjugate pairs from the given cell.
+
+    Args:
+        board (list[list[int]]): The board to check.
+        candidateCell (tuple[int, int]): The cell to check.
+
+    Returns:
+        tuple[tuple[int, int], tuple[int, int]]: The conjugate pair.
+
+    Note:
+        Empty cells are represented by the Nonetype.
+    """
+    rows = []
+    cols = []
+    boxs = []
+
+    outa = []
+    affectingsquares = getAffectingSquaresNonComplete(
+        board, candidateCell[0], candidateCell[1])
+    for square in affectingsquares:
+        if inSameRow(candidateCell, square) and value in validatposition(board, square[0], square[1]):
+            rows.append(square)
+        if inSameColumn(candidateCell, square) and value in validatposition(board, square[0], square[1]):
+            cols.append(square)
+        if inSameBox(candidateCell, square) and value in validatposition(board, square[0], square[1]):
+            boxs.append(square)
+
+    if len(rows) == 1:
+        outa.append((candidateCell, rows[0]))
+    if len(cols) == 1:
+        outa.append((candidateCell, cols[0]))
+    if len(boxs) == 1:
+        outa.append((candidateCell, boxs[0]))
+
+    return outa
+
+
+def getConjugatePairs(board: list[list[int]], candidateCells: list[tuple[int, int]], value: int) -> list[tuple[tuple[int, int], tuple[int, int]]]:
+    """Get the conjugate pairs from the given list of cells.
+
+    Args:
+        candidateCells (list[tuple[int, int]]): The list of cells to check.
+
+    Returns:
+        list[tuple[tuple[int, int], tuple[int, int]]]: A list of the conjugate pairs.
+    """
+    out = []
+    for cell in candidateCells:
+        out.extend(getConjugatePair(board, cell, value))
+
+    return out
+
+
+def zip_with_scalar(l, i):
+    return ((i, x) for x in l)
